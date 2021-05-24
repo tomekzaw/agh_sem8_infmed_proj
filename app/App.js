1767d46b53d83581c8f8ccf06af303d2850f284a
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2021-05-24 22:26:16 +0200

    Code cleanup

--- app/App.js
+++ app/App.js
@@ -7,8 +7,9 @@
  */
 
 import {Button, StyleSheet, Text, View} from 'react-native';
-import {Buffer} from 'buffer';
+
 import {BleManager} from 'react-native-ble-plx';
+import {Buffer} from 'buffer';
 import React from 'react';
 
 const bleManager = new BleManager();
@@ -16,72 +17,64 @@ const serviceUUID = '4fafc201-1fb5-459e-8fcc-c5c9c331914b';
 const characteristicUUID = 'beb5483e-36e1-4688-b7f5-ea07361b26a8';
 
 const randomInt = () => {
-  return Math.ceil(Math.random() * 100);
+  return Math.ceil(Math.random() * 1000);
 };
 
 const App = () => {
-  const [text, setText] = React.useState('');
-  const [disabled, setDisabled] = React.useState(false);
+  const asyncHandleConnect = async (error, device) => {
+    if (error) {
+      // TODO: handle Bluetooth adapter or Location off
+      console.log(JSON.stringify(error));
+      return;
+    }
+
+    try {
+      await bleManager.stopDeviceScan();
+      console.log('Stopped device scan');
+
+      await bleManager.connectToDevice(device.id);
+      console.log('Connected to device');
+
+      await device.discoverAllServicesAndCharacteristics();
+      console.log('Discovered services and characteristics');
+
+      const [min, max] = [randomInt(), randomInt()].sort();
+      const request = {min, max};
+      console.log(`Generated request: ${JSON.stringify(request)}`);
+
+      await device.writeCharacteristicWithResponseForService(
+        serviceUUID,
+        characteristicUUID,
+        Buffer.from(JSON.stringify(request)).toString('base64'),
+      );
+      console.log('Wrote request to characteristic');
+
+      const characteristic = await device.readCharacteristicForService(
+        serviceUUID,
+        characteristicUUID,
+      );
+      console.log('Read response from characteristic');
+
+      const response = JSON.parse(
+        Buffer.from(characteristic.value, 'base64').toString(),
+      );
+      console.log(`Parsed response: ${JSON.stringify(response)}`);
+    } catch (e) {
+      console.log('Error');
+    } finally {
+      await device.cancelConnection();
+      console.log('Cancelled connection');
+    }
+  };
 
   const handlePress = async () => {
-    console.log('Scan in progress...');
-    setText('Scan in progress...');
-    setDisabled(true);
-
-    await bleManager.startDeviceScan(
-      [serviceUUID],
-      null,
-      async (error, device) => {
-        if (error) {
-          // TODO: handle Bluetooth adapter or Location off
-          console.log(JSON.stringify(error));
-          setText('Scan error');
-          setDisabled(false);
-          return;
-        }
-
-        await bleManager.stopDeviceScan();
-
-        await bleManager.connectToDevice(device.id);
-        console.log(device.name);
-
-        await device.discoverAllServicesAndCharacteristics();
-
-        let jsonToSend = {
-          functionName: 'ADD',
-          args: [randomInt(), randomInt()],
-        };
-
-        let buff = new Buffer(JSON.stringify(jsonToSend));
-
-        await device.writeCharacteristicWithResponseForService(
-          serviceUUID,
-          characteristicUUID,
-          buff.toString('base64'),
-        );
-
-        const characteristic = await device.readCharacteristicForService(
-          serviceUUID,
-          characteristicUUID,
-        );
-
-        let b = new Buffer(characteristic.value, 'base64');
-        let res = JSON.parse(b.toString());
-        console.log(res);
-
-        await device.cancelConnection();
-
-        console.log('Scan finished');
-        setText('Scan finished');
-        setDisabled(false);
-      },
-    );
+    await bleManager.startDeviceScan([serviceUUID], null, asyncHandleConnect);
+    console.log('Started device scan');
   };
 
   return (
     <View style={styles.containerStyle}>
-      <Button title="Click me!" onPress={handlePress} disabled={disabled} />
-      <Text>{text}</Text>
+      <Button title="Click me!" onPress={handlePress} />
     </View>
   );
 };
--- node-ble-server/main.js
+++ node-ble-server/main.js
@@ -1,93 +1,71 @@
-let bleno = require('@abandonware/bleno');
+let bleno = require("@abandonware/bleno");
 
-
-let SERVICE_UUID = "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
-let CHARACTERISTIC_UUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8"
-let NAME = 'HELLO FRIEND'
-
-
-let exampleFunctions = {
-    'ADD': (a, b) => a + b
-}
-
-let funRes = null
+const serviceUUID = "4fafc201-1fb5-459e-8fcc-c5c9c331914b";
+const characteristicUUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8";
+const name = "HELLO FRIEND";
 
 let Characteristic = bleno.Characteristic;
 
-function onReadRequest(offset, callback) {
-    console.log("Received read request")
+var prevRequest = null;
 
-    let output = {
-        result: funRes
-    }
-
-    console.log(output)
-
-    callback(Characteristic.RESULT_SUCCESS, new Buffer(JSON.stringify(output), 'ascii'));
+function processRequest(request) {
+  const { min, max } = request;
+  return { sum: min + max };
 }
 
 function onWriteRequest(data, offset, withoutResponse, callback) {
-    let encoded = data.toString('utf8')
-    let json = JSON.parse(encoded)
+  const request = JSON.parse(data.toString("utf8"));
+  prevRequest = request;
+  console.log(request);
 
-    console.log("Received write request")
-    console.log(json)
+  callback(Characteristic.RESULT_SUCCESS);
+}
 
-    let funName = json.functionName
-    funRes = exampleFunctions[funName](...json.args)
+function onReadRequest(offset, callback) {
+  const response = processRequest(prevRequest);
+  console.log(response);
 
-    callback(Characteristic.RESULT_SUCCESS);
-
+  callback(
+    Characteristic.RESULT_SUCCESS,
+    Buffer.from(JSON.stringify(response), "ascii")
+  );
 }
 
 let characteristic = new Characteristic({
-    uuid: CHARACTERISTIC_UUID, // or 'fff1' for 16-bit
-    properties: ['read', 'write', 'notify', 'indicate'], // can be a combination of 'read', 'write', 'writeWithoutResponse', 'notify', 'indicate'
-    value: null, // optional static value, must be of type Buffer - for read only characteristics
-    descriptors: [
-        new bleno.Descriptor({
-            uuid: '2902',
-            value: 'Hello word'
-        })
-    ],
-    onReadRequest: onReadRequest, // optional read request handler, function(offset, callback) { ... }
-    onWriteRequest: onWriteRequest, // optional write request handler, function(data, offset, withoutResponse, callback) { ...}
-    onSubscribe: null, // optional notify/indicate subscribe handler, function(maxValueSize, updateValueCallback) { ...}
-    onUnsubscribe: null, // optional notify/indicate unsubscribe handler, function() { ...}
-    onNotify: null, // optional notify sent handler, function() { ...}
-    onIndicate: null // optional indicate confirmation received handler, function() { ...}
+  uuid: characteristicUUID,
+  properties: ["read", "write", "notify", "indicate"],
+  value: null,
+  descriptors: [
+    new bleno.Descriptor({
+      uuid: "2902",
+      value: "Hello word",
+    }),
+  ],
+  onReadRequest: onReadRequest,
+  onWriteRequest: onWriteRequest,
+  onSubscribe: null,
+  onUnsubscribe: null,
+  onNotify: null,
+  onIndicate: null,
 });
-
 
 let PrimaryService = bleno.PrimaryService;
 
 let primaryService = new PrimaryService({
-    uuid: SERVICE_UUID,
-    characteristics: [
-        characteristic
-    ]
+  uuid: serviceUUID,
+  characteristics: [characteristic],
 });
 
-
-console.log('Starting BLE server');
-
-bleno.on('stateChange', function (state) {
-    console.log('on -> stateChange: ' + state);
-
-    if (state === 'poweredOn') {
-        bleno.startAdvertising(NAME, [SERVICE_UUID]);
-    } else {
-        bleno.stopAdvertising();
-    }
+bleno.on("stateChange", function (state) {
+  if (state === "poweredOn") {
+    bleno.startAdvertising(name, [serviceUUID]);
+  } else {
+    bleno.stopAdvertising();
+  }
 });
 
-
-bleno.on('advertisingStart', function (error) {
-    console.log('on -> advertisingStart: ' + (error ? 'error ' + error : 'success'));
-
-    if (!error) {
-        bleno.setServices([
-            primaryService
-        ]);
-    }
+bleno.on("advertisingStart", function (error) {
+  if (!error) {
+    bleno.setServices([primaryService]);
+  }
 });
